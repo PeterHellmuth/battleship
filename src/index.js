12e90c042669133c1a0ff6342ea446d348a9f54b
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-11 13:54:15 -0500

    Add receiveAttack and allShipsSunk, tests pass

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import "./style.css";
 
 class Ship {
-  constructor(length) {
+  constructor(length, isVertical) {
     this.length = length;
     this.hitArr = Array.apply(null, Array(length));
+    this.isVertical = isVertical;
   }
 
   hit(location) {
     if (location >= 0 && location < this.hitArr.length) {
-      if (this.hitArr[location] == null) {
+      if (!this.hitArr[location]) {
         this.hitArr[location] = true;
         return true;
       } else {
         return false;
       }
     }
-    return null;
+    return false;
   }
   isSunk() {
     let sunk = true;
@@ -28,4 +29,123 @@ class Ship {
   }
 }
 
-export { Ship };
+class GameBoard {
+  constructor(size) {
+    this.grid = new Array(size)
+      .fill(null)
+      .map(() => new Array(size).fill(null));
+    this.size = size;
+  }
+
+  placeShip(x, y, length, isVertical = true) {
+    if (isVertical) {
+      if (length > 0 && length + y <= this.size) {
+        let existingShip = false;
+        for (let i = y; i < y + length; i++) {
+          if (this.grid[x][i]) {
+            existingShip = true;
+          }
+        }
+
+        if (!existingShip) {
+          let newShip = new Ship(length, isVertical);
+          for (let i = y; i < y + length; i++) {
+            this.grid[x][i] = newShip;
+          }
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } else {
+      //horizontal
+      if (length > 0 && length + x <= this.size) {
+        let existingShip = false;
+        for (let i = x; i < x + length; i++) {
+          if (this.grid[i][y]) {
+            existingShip = true;
+          }
+        }
+
+        if (!existingShip) {
+          let newShip = new Ship(length, isVertical);
+          for (let i = x; i < x + length; i++) {
+            this.grid[i][y] = newShip;
+          }
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+  receiveAttack(x, y) {
+    if (this.grid[x][y]) {
+      //there is a ship at this location, send hit signal and check if it's already hit.
+      let shipAttacked = this.grid[x][y];
+      let shipOrigin = this.findShipCoordinate(shipAttacked);
+      if (shipAttacked.isVertical) {
+        return shipAttacked.hit(y - shipOrigin[1]);
+      } else {
+        //Horizontal
+        return shipAttacked.hit(x - shipOrigin[0]);
+      }
+    } else {
+      //no ship, check if we've already missed here.
+      if (this.grid[x][y] == null) {
+        this.grid[x][y] = false;
+        return true; //miss recorded
+      } else {
+        return false; //already missed here, can't attack here.
+      }
+    }
+  }
+
+  findShipCoordinate(ship) {
+    let isVertical = ship.isVertical;
+
+    if (ship) {
+      if (isVertical) {
+        for (let i = 0; i < this.size; i++) {
+          for (let j = 0; j < this.size; j++) {
+            if (this.grid[i][j] === ship) {
+              return [i, j];
+            }
+          }
+        }
+      } else {
+        //Horizontal
+        for (let j = 0; j < this.size; j++) {
+          for (let i = 0; i < this.size; i++) {
+            if (this.grid[i][j] === ship) {
+              return [i, j];
+            }
+          }
+        }
+      }
+    }
+
+    return null;
+  }
+
+  allShipsSunk() {
+    let allShipsSunk = true;
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (this.grid[i][j]) {
+          if (!this.grid[i][j].isSunk()) {
+            allShipsSunk = false;
+          }
+        }
+      }
+    }
+    return allShipsSunk;
+  }
+}
+
+export { Ship, GameBoard };
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,13 @@
-import { Ship } from ".";
+import { Ship, GameBoard } from ".";
 
 const aShip = new Ship(5);
+const gameBoard = new GameBoard(10);
 
 test("Receives a hit one time in each location. Doesn't accept outside range.", () => {
   expect(aShip.hit(1)).toBe(true);
   expect(aShip.hit(1)).toBe(false);
-  expect(aShip.hit(-1)).toBe(null);
-  expect(aShip.hit(5)).toBe(null);
+  expect(aShip.hit(-1)).toBe(false);
+  expect(aShip.hit(5)).toBe(false);
 });
 
 test("Reports sunk status.", () => {
@@ -17,4 +18,31 @@ test("Reports sunk status.", () => {
   aShip.hit(3);
   aShip.hit(4);
   expect(aShip.isSunk()).toBe(true);
+});
+
+test("Places ship on gameboard if it's within bounds.", () => {
+  expect(gameBoard.placeShip(9, 0, 10, true)).toBe(true);
+  expect(gameBoard.placeShip(8, 1, 10, true)).toBe(false);
+});
+
+test("Won't place a ship on top of another ship.", () => {
+  expect(gameBoard.placeShip(0, 5, 10, false)).toBe(false);
+  expect(gameBoard.placeShip(0, 5, 9, false)).toBe(true);
+});
+
+test("Records hit on ships or miss in sea, only one time.", () => {
+  expect(gameBoard.receiveAttack(1, 5)).toBe(true); //ship
+  expect(gameBoard.receiveAttack(1, 5)).toBe(false);
+  expect(gameBoard.receiveAttack(0, 4)).toBe(true); //sea
+  expect(gameBoard.receiveAttack(0, 4)).toBe(false);
+});
+
+test("Can report if all ships are sunk or not.", () => {
+  expect(gameBoard.allShipsSunk()).toBe(false);
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      gameBoard.receiveAttack(i, j);
+    }
+  }
+  expect(gameBoard.allShipsSunk()).toBe(true);
 });
