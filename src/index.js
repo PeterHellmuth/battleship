6a552b1e5b6af38a97857fd95bba7d2f03cd0c21
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-11 11:13:14 -0500

    Add ship class hit and isSunk methods, passed test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import "./style.css";
 
-function sum(a, b) {
-  return a + b;
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hitArr = Array.apply(null, Array(length));
+  }
+
+  hit(location) {
+    if (location >= 0 && location < this.hitArr.length) {
+      if (this.hitArr[location] == null) {
+        this.hitArr[location] = true;
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return null;
+  }
+  isSunk() {
+    let sunk = true;
+    this.hitArr.forEach((hit) => {
+      if (hit == null) {
+        sunk = false;
+      }
+    });
+    return sunk;
+  }
 }
 
-export { sum };
+export { Ship };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,20 @@
-import { sum } from ".";
+import { Ship } from ".";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+const aShip = new Ship(5);
+
+test("Receives a hit one time in each location. Doesn't accept outside range.", () => {
+  expect(aShip.hit(1)).toBe(true);
+  expect(aShip.hit(1)).toBe(false);
+  expect(aShip.hit(-1)).toBe(null);
+  expect(aShip.hit(5)).toBe(null);
+});
+
+test("Reports sunk status.", () => {
+  expect(aShip.isSunk()).toBe(false);
+  aShip.hit(0);
+  aShip.hit(1);
+  aShip.hit(2);
+  aShip.hit(3);
+  aShip.hit(4);
+  expect(aShip.isSunk()).toBe(true);
 });
