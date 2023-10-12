87ca95be7e2be929483e2af5515c50686773f535
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-12 12:16:57 -0500

    Add game loop, Split DOM update from generate

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,5 @@
+import { cellHovered, cellClicked } from ".";
+
 const playerOneGameboardContainer = document.getElementById(
   "player-one-gameboard",
 );
@@ -6,17 +8,78 @@ const playerTwoGameboardContainer = document.getElementById(
   "player-two-gameboard",
 );
 
-function generateGameBoardDOM(gameBoard) {
+function updateGameBoardDOM(
+  gameBoard,
+  id,
+  phantomBoat = null,
+  phantomBoatLength = null,
+  phantomBoatVertical = null,
+  hideShips = null,
+) {
+  const { size } = gameBoard;
+  const phantomBoatCells = [];
+
+  if (phantomBoat) {
+    if (phantomBoatVertical) {
+      for (let i = 0; i < phantomBoatLength; i++) {
+        phantomBoatCells.push([phantomBoat[0], phantomBoat[1] + i]);
+      }
+    } else {
+      for (let i = 0; i < phantomBoatLength; i++) {
+        phantomBoatCells.push([phantomBoat[0] + i, phantomBoat[1]]);
+      }
+    }
+  }
+
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      const cell = gameBoard.grid[x][y];
+      const domCell = document.getElementById(`${id + x}:${y}`);
+      domCell.className = "cell";
+      if (phantomBoat) {
+        phantomBoatCells.forEach((phantomCell) => {
+          if (phantomCell[0] === x && phantomCell[1] === y) {
+            domCell.classList.add("ship");
+          }
+        });
+      }
+      if (cell) {
+        if (gameBoard.cellIsHitShip(x, y)) {
+          domCell.classList.add("hit-ship");
+        } else if (hideShips) {
+          domCell.classList.add("empty");
+        } else {
+          domCell.classList.add("ship");
+        }
+      } else if (cell === false) {
+        domCell.classList.add("miss");
+      } else {
+        domCell.classList.add("empty");
+      }
+    }
+  }
+}
+
+function generateGameBoardDOM(gameBoard, id, hideShips = null) {
   const returnElement = document.createElement("div");
   returnElement.classList.add("game-board");
   const { size } = gameBoard;
+
   for (let y = size - 1; y >= 0; y--) {
     for (let x = 0; x < size; x++) {
       const cell = gameBoard.grid[x][y];
       const newCell = document.createElement("span");
+      newCell.id = `${id}${x}:${y}`;
+      newCell.addEventListener("mouseover", cellHovered);
+      newCell.addEventListener("click", cellClicked);
       newCell.classList.add("cell");
+
       if (cell) {
-        newCell.classList.add("ship");
+        if (hideShips) {
+          newCell.classList.add("ship");
+        } else {
+          newCell.classList.add("empty");
+        }
       } else if (cell === false) {
         newCell.classList.add("miss");
       } else {
@@ -34,16 +97,33 @@ function clearChildren(element) {
   }
 }
 
-function updateDOM(playerOne, playerTwo) {
+function generateDOM(playerOne, playerTwo) {
   clearChildren(playerOneGameboardContainer);
   clearChildren(playerTwoGameboardContainer);
 
   playerOneGameboardContainer.appendChild(
-    generateGameBoardDOM(playerOne.gameBoard),
+    generateGameBoardDOM(playerOne.gameBoard, "playerOne"),
   );
   playerTwoGameboardContainer.appendChild(
-    generateGameBoardDOM(playerTwo.gameBoard),
+    generateGameBoardDOM(playerTwo.gameBoard, "playerTwo"),
   );
 }
+function updateDOM(
+  playerOne,
+  playerTwo,
+  phantomBoat = null,
+  phantomBoatLength = null,
+  phantomBoatVertical = null,
+) {
+  updateGameBoardDOM(
+    playerOne.gameBoard,
+    "playerOne",
+    phantomBoat,
+    phantomBoatLength,
+    phantomBoatVertical,
+  );
 
-export { updateDOM };
+  updateGameBoardDOM(playerTwo.gameBoard, "playerTwo", null, null, null, true);
+}
+
+export { updateDOM, generateDOM };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-classes-per-file */
 import "./style.css";
-import { updateDOM } from "./dom.js";
+import { updateDOM, generateDOM } from "./dom.js";
 
 const BOARD_SIZE = 10;
 
@@ -12,15 +13,19 @@ class Ship {
 
   hit(location) {
     if (location >= 0 && location < this.hitArr.length) {
-      if (!this.hitArr[location]) {
+      if (!this.isHit(location)) {
         this.hitArr[location] = true;
         return true;
-      } else {
-        return false;
       }
+      return false;
     }
     return false;
   }
+
+  isHit(location) {
+    return this.hitArr[location];
+  }
+
   isSunk() {
     let sunk = true;
     this.hitArr.forEach((hit) => {
@@ -51,66 +56,70 @@ class GameBoard {
         }
 
         if (!existingShip) {
-          let newShip = new Ship(length, isVertical);
+          const newShip = new Ship(length, isVertical);
           for (let i = y; i < y + length; i++) {
             this.grid[x][i] = newShip;
           }
           return true;
-        } else {
-          return false;
         }
-      } else {
         return false;
       }
-    } else {
-      //horizontal
-      if (length > 0 && length + x <= this.size) {
-        let existingShip = false;
-        for (let i = x; i < x + length; i++) {
-          if (this.grid[i][y]) {
-            existingShip = true;
-          }
-        }
-
-        if (!existingShip) {
-          let newShip = new Ship(length, isVertical);
-          for (let i = x; i < x + length; i++) {
-            this.grid[i][y] = newShip;
-          }
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
+      return false;
     }
+    // horizontal
+    if (length > 0 && length + x <= this.size) {
+      let existingShip = false;
+      for (let i = x; i < x + length; i++) {
+        if (this.grid[i][y]) {
+          existingShip = true;
+        }
+      }
+
+      if (!existingShip) {
+        const newShip = new Ship(length, isVertical);
+        for (let i = x; i < x + length; i++) {
+          this.grid[i][y] = newShip;
+        }
+        return true;
+      }
+      return false;
+    }
+    return false;
   }
 
   receiveAttack(x, y) {
     if (this.grid[x][y]) {
-      //there is a ship at this location, send hit signal and check if it's already hit.
-      let shipAttacked = this.grid[x][y];
-      let shipOrigin = this.findShipCoordinate(shipAttacked);
+      // there is a ship at this location, send hit signal and check if it's already hit.
+      const shipAttacked = this.grid[x][y];
+      const shipOrigin = this.findShipCoordinate(shipAttacked);
       if (shipAttacked.isVertical) {
         return shipAttacked.hit(y - shipOrigin[1]);
-      } else {
-        //Horizontal
-        return shipAttacked.hit(x - shipOrigin[0]);
       }
-    } else {
-      //no ship, check if we've already missed here.
-      if (this.grid[x][y] == null) {
-        this.grid[x][y] = false;
-        return true; //miss recorded
-      } else {
-        return false; //already missed here, can't attack here.
-      }
+      // Horizontal
+      return shipAttacked.hit(x - shipOrigin[0]);
     }
+    // no ship, check if we've already missed here.
+    if (this.grid[x][y] == null) {
+      this.grid[x][y] = false;
+      return true; // miss recorded
+    }
+    return false; // already missed here, can't attack here.
+  }
+
+  cellIsHitShip(x, y) {
+    if (this.grid[x][y]) {
+      const ship = this.grid[x][y];
+      const shipOrigin = this.findShipCoordinate(ship);
+      if (ship.isVertical) {
+        return ship.isHit(y - shipOrigin[1]);
+      }
+      return ship.isHit(x - shipOrigin[0]);
+    }
+    return false;
   }
 
   findShipCoordinate(ship) {
-    let isVertical = ship.isVertical;
+    const { isVertical } = ship;
 
     if (ship) {
       if (isVertical) {
@@ -122,7 +131,7 @@ class GameBoard {
           }
         }
       } else {
-        //Horizontal
+        // Horizontal
         for (let j = 0; j < this.size; j++) {
           for (let i = 0; i < this.size; i++) {
             if (this.grid[i][j] === ship) {
@@ -159,18 +168,139 @@ class Player {
   }
 }
 
-let playerOne = new Player(null, "Player One");
-let playerTwo = new Player(playerOne, "Player Two");
+const playerOne = new Player(null, "Player One");
+const playerTwo = new Player(playerOne, "Player Two");
 playerTwo.opponent = playerOne;
-
+const gameText = document.getElementById("game-text");
 let winner = false;
 let currentPlayer = playerOne;
+const { body } = document;
+window.addEventListener("contextmenu", (e) => e.preventDefault());
+body.addEventListener("contextmenu", rightClick);
 
-playerOne.gameBoard.placeShip(0, 0, 5, true);
-//while (!winner) {
-//console.log("It is player one's turn");
-//}
+let boats = [5, 4, 3, 3, 2];
 
-updateDOM(playerOne, playerTwo);
+// AI place.
+while (boats.length >= 1) {
+  const randX = Math.floor(Math.random() * BOARD_SIZE);
+  const randY = Math.floor(Math.random() * BOARD_SIZE);
+  const isVertical = Math.random() < 0.5;
+  if (
+    playerTwo.gameBoard.placeShip(
+      randX,
+      randY,
+      boats[boats.length - 1],
+      isVertical,
+    )
+  ) {
+    boats.pop();
+  }
+}
 
-export { Ship, GameBoard };
+boats = [5, 4, 3, 3, 2];
+let placingBoats = true;
+let currentIsVertical = true;
+
+gameText.innerText = "Place your boats, right click to rotate.";
+
+generateDOM(playerOne, playerTwo);
+
+function randomCoord() {
+  return Math.floor(Math.random() * BOARD_SIZE);
+}
+
+function aiPlayerTurn() {
+  let x = randomCoord();
+  let y = randomCoord();
+  while (!playerOne.gameBoard.receiveAttack(x, y)) {
+    x = randomCoord();
+    y = randomCoord();
+  }
+
+  if (playerOne.gameBoard.allShipsSunk()) {
+    winner = playerTwo;
+    gameText.innerText = "Player two wins!";
+  }
+  currentPlayer = playerOne;
+}
+
+function cellHovered(event) {
+  const cell = event.target;
+  const { id } = cell;
+  const x = Number(id.slice(9, 10));
+  const y = Number(id.slice(11, 12));
+  if (placingBoats) {
+    updateDOM(
+      playerOne,
+      playerTwo,
+      [x, y],
+      boats[boats.length - 1],
+      currentIsVertical,
+    );
+  }
+}
+
+function cellClicked(event) {
+  if (!winner) {
+    const cell = event.target;
+    const { id } = cell;
+    const x = Number(id.slice(9, 10));
+    const y = Number(id.slice(11, 12));
+    if (placingBoats) {
+      if (id.slice(0, 9) === "playerOne") {
+        if (
+          playerOne.gameBoard.placeShip(
+            x,
+            y,
+            boats[boats.length - 1],
+            currentIsVertical,
+          )
+        ) {
+          boats.pop();
+          if (boats.length === 0) {
+            placingBoats = false;
+            gameText.innerText = "Player one, fire!";
+          }
+          updateDOM(playerOne, playerTwo);
+        }
+      }
+    } else if (currentPlayer === playerOne) {
+      if (id.slice(0, 9) === "playerTwo") {
+        if (playerTwo.gameBoard.receiveAttack(x, y)) {
+          if (playerTwo.gameBoard.allShipsSunk()) {
+            winner = playerOne;
+            gameText.innerText = "Player one wins!";
+          } else {
+            currentPlayer = playerTwo;
+            aiPlayerTurn();
+          }
+          updateDOM(
+            playerOne,
+            playerTwo,
+            [x, y],
+            boats[boats.length - 1],
+            currentIsVertical,
+          );
+        }
+      }
+    }
+  }
+}
+
+function rightClick(event) {
+  const cell = event.target;
+  const { id } = cell;
+  const x = Number(id.slice(9, 10));
+  const y = Number(id.slice(11, 12));
+  if (placingBoats) {
+    currentIsVertical = !currentIsVertical;
+    updateDOM(
+      playerOne,
+      playerTwo,
+      [x, y],
+      boats[boats.length - 1],
+      currentIsVertical,
+    );
+  }
+}
+export { Ship, GameBoard, cellHovered, cellClicked };
